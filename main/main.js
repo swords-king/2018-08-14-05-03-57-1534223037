a0a25cc759431a8c737b58aa7ba45187e7ec56d6
swords-king <zzy11235@qq.com>
2018-09-23 22:37:38 +0800

    homework for 2_3

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
 module.exports = function main() {
   // Write your code here
+  var sum = 0;
+  var str = arguments[0]
+  for (var x of str){
+	  var a = parseInt(x);
+	  sum += a;
+  }
+  return sum;
 };
